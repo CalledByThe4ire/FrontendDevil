242dfa4828834aaec54081112128e49800480104
Andrey Karamnoff <karamnoff@gmail.com>
2016-06-10 09:07:36 +0300

    :tada: Initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Frontend-devil
+###Devil boilerplate for html+js+css apps
+
+Move to folder
+
+Run "npm i", "bower i", "npm run build"
+
+Open "build/index.html"
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/html5shiv/dist/html5shiv.js
+//= ../../bower_components/picturefill/dist/picturefill.js
+//= ../../bower_components/object-fit-images/dist/ofi.browser.js
+
