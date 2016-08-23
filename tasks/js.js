500a5104a2a953360b6608106e2ce57e478c9649
Andrey Karamnoff <karamnoff@gmail.com>
2016-08-23 20:31:08 +0300

    :arrow_down: Remove bower from package.json

--- tasks/js.js
+++ tasks/js.js
@@ -11,39 +11,47 @@ const gulp = require("gulp"),
   sourcemaps = require("gulp-sourcemaps"),
   combiner = require("stream-combiner2").obj;
 
-const env= !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+const env = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 
 
-
-//module.exports = function(options) {
-//  return function() {
-//    return combiner(
-//      browserify(options.src).bundle(),
-//      source(options.bundle),
-//      buffer(),
-//      gulpIf( !env, combiner( uglify(), rename("main.min.js") ) ),
-//      sourcemaps.init({loadMaps: true}),
-//      sourcemaps.write(options.maps),
-//      gulp.dest(options.dst)
-//    ).on("error", notify.onError(
-//      function(err) {
-//        return {
-//          title: "Javascript",
-//          message: err.message
-//        };
-//      }
-//    ));
-//  };
-//};
+// module.exports = function(options) {
+//   return function() {
+//     return combiner(
+//       browserify(options.src).bundle(),
+//       source(options.bundle),
+//       buffer(),
+//       gulpIf(!env, combiner(uglify(), rename("main.min.js"))),
+//       sourcemaps.init({loadMaps: true}),
+//       sourcemaps.write(options.maps),
+//       gulp.dest(options.dst)
+//     ).on("error", notify.onError(
+//       function(err) {
+//         return {
+//           title: "Javascript",
+//           message: err.message
+//         };
+//       }
+//     ));
+//   };
+// };
 
 module.exports = function(options) {
-    return function() {
-        return browserify(options.src)
-            .bundle()
-            .pipe(source(options.bundle))
-            .pipe(buffer())
-            .pipe(sourcemaps.init({loadMaps: true}))
-            .pipe(sourcemaps.write(options.maps))
-            .pipe(gulp.dest(options.dst));
-    };
+  return function() {
+    return browserify(options.src)
+      .bundle()
+      .on("error", notify.onError(
+        function(err) {
+          return {
+            title: "Javascript",
+            message: err.message
+          };
+        }
+      ))
+      .pipe(source(options.bundle))
+      .pipe(buffer())
+      .pipe( gulpIf(!env, combiner(uglify(), rename("main.min.js"))) )
+      .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(sourcemaps.write(options.maps))
+      .pipe(gulp.dest(options.dst));
+  };
 };
