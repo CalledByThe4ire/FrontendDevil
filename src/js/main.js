b2ae3c0b4ab5ff78f987ef7a5749fb07fc93e02a
Andrey Karamnoff <karamnoff@gmail.com>
2016-08-23 16:25:49 +0300

    :art: Replace links to js libraries via “require” method

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
-//= ../../bower_components/jquery/dist/jquery.js
-//= ../../bower_components/html5shiv/dist/html5shiv.js
-//= ../../bower_components/picturefill/dist/picturefill.js
-//= ../../bower_components/object-fit-images/dist/ofi.browser.js
-
+require("jquery");
+require("html5shiv");
+require("picturefill");
+require("object-fit-images");
